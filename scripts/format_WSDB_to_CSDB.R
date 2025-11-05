eb38a7ef13de534cdfff1f6eeb04dd540ffc100a
joystanistreet <joy.stanistreet@dfo-mpo.gc.ca>
2025-11-05 14:55:29 -0400

    Update format_WSDB_to_CSDB.R
    Added flexibility for output options, added code for handling dates and behaviour descriptions to this script rather than the WSDB query

--- scripts/format_WSDB_to_CSDB.R
+++ scripts/format_WSDB_to_CSDB.R
@@ -1,24 +1,34 @@
 
 # Script to format data exported from WSDB and prepare for batch input to CSDB 
 
-# Input:
-  ## .csv file with data records exported from WSDB
+# INPUT:
+  ## one .csv file with data records exported from WSDB
 
-# Output:
-  ## one .xlsx file per data source for data source codes with >100 records
-  ## one .xlsx file labeled "MULTI" including all data source codes with <100 records
+# OUTPUT OPTIONS:
+
+  # If create_batch_files = TRUE:
+
+    ## subfolder containing:
+      ### one .xlsx file per data source for data source codes with >100 records
+      ### one .xlsx file labeled "MULTI" including all data source codes with <100 records
 
     ## note that the criteria outlined above also apply to the "UNKNOWN" data source code ("0")
 
+  # If create_batch_files = FALSE:
+
+    ## one .xlsx file containing all records from the input file
 
 # developed by M. Murphy 2024-02-16; modified by J. Stanistreet 2024-05-30
 
 #####################################################
 
-### TO RUN SCRIPT, MODIFY THIS LINE:
+### TO RUN SCRIPT, MODIFY THESE LINES:
 
-# specify csv input file
-filename <- "2014"
+# specify csv input file (without including ".csv")
+filename <- "2000export"
+
+# select whether to create batch output files split by data source or single output matching input file
+create_batch_files = FALSE
 
 # run code below
 
@@ -35,37 +45,55 @@ input_directory <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\CSDB\WSDB_
 # Set output directory
 output_directory = r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\CSDB\WSDB_to_CSDB\Formatted_for_CSDB\)"
 
-# Check if output subfolder exists & create it if needed
-if(!dir.exists(paste0(output_directory, filename)))
+# If create_batch is TRUE, check if output subfolder exists & create it if needed
+if(create_batch_files) {
+  if(!dir.exists(paste0(output_directory, filename)))
    {
   dir.create(paste0(output_directory, filename))
   }
+  output_subfolder = paste0(output_directory, filename, "\\")
+} else {
+  output_subfolder = output_directory
+}
 
-output_subfolder = paste0(output_directory, filename, "\\")
-
-# Set data table directory
+# Set code table directory
 code_table_directory <- r"(R:\Science\CetaceanOPPNoise\CetaceanOPPNoise_12\CSDB\WSDB_to_CSDB\Code_tables\)"
 
-# Load data tables
+# Load code tables
 Species_code_table <- read_csv(paste0(code_table_directory, "Species_code_table.csv"))
 Activity_type_code_table <- read_csv(paste0(code_table_directory, "Activity_type_code_table.csv"))
 Effort_code_table <- read_csv(paste0(code_table_directory, "Effort_code_table.csv"))
 Data_source_code_table <- read_csv(paste0(code_table_directory, "Data_source_code_table.csv"))
+
+# Load behaviour code table and add extra columns for CD1 through CD5 and DESC1 through DESC5
+Behaviour_code_table <- read_csv(paste0(code_table_directory, "Behaviour_code_table.csv")) %>% 
+  mutate(BEHAVIOUR_CD1 = BEHAVIOUR_CD,
+         BEHAVIOUR_CD2 = BEHAVIOUR_CD,
+         BEHAVIOUR_CD3 = BEHAVIOUR_CD,
+         BEHAVIOUR_CD4 = BEHAVIOUR_CD,
+         BEHAVIOUR_CD5 = BEHAVIOUR_CD,
+         BEHAVIOUR_DESC_1 = BEHAVIOUR_DESC,
+         BEHAVIOUR_DESC_2 = BEHAVIOUR_DESC,
+         BEHAVIOUR_DESC_3 = BEHAVIOUR_DESC,
+         BEHAVIOUR_DESC_4 = BEHAVIOUR_DESC)
+
+# Load CSDB fields template
+#CSDB_template<-read_csv(paste0(code_table_directory, "WSDBtoCSDB_template.csv"))
 
 # Load WSDB data
 WSDB_data <- read_csv(paste0(input_directory, filename, ".csv"),
                       locale = readr::locale(encoding = "Cp1252"))
 
 # Turn off scientific notation
-options(scipen=999)
+options(scipen = 999)
 
 ##### PART 2: FORMAT DATA FOR CSDB #####
 
-# 1) Format Year Month and Day to pull from UTC_Year, UTC_Month, UTC_Day when available, if not use Reported_Year, Reported_Month, Reported_Day
+# 1) Format Year Month and Day to pull from WS_DATETIME_UTC when available, otherwise use WS_DATETIME
 CSDB_data <- WSDB_data %>%
-  mutate(Year= ifelse(is.na(UTC_Year), Reported_Year, UTC_Year),
-         Month= ifelse(is.na(UTC_Month), Reported_Month, UTC_Month),
-         Day= ifelse(is.na(UTC_Day), Reported_Day, UTC_Day))
+  mutate(Year = ifelse(is.na(as_date(WS_DATETIME_UTC)), year(as_date(WS_DATETIME)), year(as_date(WS_DATETIME_UTC))),
+         Month = ifelse(is.na(as_date(WS_DATETIME_UTC)), month(as_date(WS_DATETIME)), month(as_date(WS_DATETIME_UTC))),
+         Day = ifelse(is.na(as_date(WS_DATETIME_UTC)), day(as_date(WS_DATETIME)), day(as_date(WS_DATETIME_UTC))))
 
 # 2) Modify WS_EVENT_ID to Regional_Primary_Key with first 2 digits “11” (representing MAR Region)
 # and the total number of digits is 11 (including the WS_EVENT_ID)
@@ -134,7 +162,14 @@ CSDB_data <- CSDB_data %>%
   mutate(Count_Uncertainty = case_when(is.na(COUNT_UNCERTAINTY_CD) ~ 0,
                                         TRUE ~ as.numeric(COUNT_UNCERTAINTY_CD)))
 
-# 12) Create column called Behaviour_Comments that concatenates the five BEHAVIOUR_DESC columns into the one column with hyphens in between
+# 12) Map behaviour codes to descriptions using Behaviour_code_table
+CSDB_data = CSDB_data %>%
+  left_join(select(Behaviour_code_table, BEHAVIOUR_CD1, BEHAVIOUR_DESC), by = "BEHAVIOUR_CD1") %>% 
+  left_join(select(Behaviour_code_table, BEHAVIOUR_CD2, BEHAVIOUR_DESC_1), by = "BEHAVIOUR_CD2") %>% 
+  left_join(select(Behaviour_code_table, BEHAVIOUR_CD3, BEHAVIOUR_DESC_2), by = "BEHAVIOUR_CD3") %>% 
+  left_join(select(Behaviour_code_table, BEHAVIOUR_CD4, BEHAVIOUR_DESC_3), by = "BEHAVIOUR_CD4") %>%
+  left_join(select(Behaviour_code_table, BEHAVIOUR_CD5, BEHAVIOUR_DESC_4), by = "BEHAVIOUR_CD5")
+
 # 12a) Change 'NOT RECORDED' to NA from BEHAVIOUR_DESC columns
 CSDB_data = CSDB_data %>%
   mutate(across(starts_with("BEHAVIOUR_DESC"),~ case_when(. == "NOT RECORDED" ~ NA_character_,
@@ -191,7 +226,10 @@ CSDB_data_formatted <- CSDB_data %>%
 
 ##### PART 3: EXPORT FOR BATCH UPLOAD TO CSDB #####
 
-# Filter for data sources with >= 100 records
+# If create_batch_files = TRUE, create batch output files by data source, otherwise create single output
+
+if (create_batch_files){
+  # Filter for data sources with >= 100 records
 large_data_source <- CSDB_data_formatted %>% 
   group_by(Data_Source_Code) %>% 
   filter(n() >= 100) %>% 
@@ -230,4 +268,9 @@ lapply(names(export_list), function (x) {
              ))
 })
 
+} else {
+  
+  write_xlsx(CSDB_data_formatted,
+             path = paste0(output_subfolder, format(Sys.Date(), "%Y%m%d"), "_", str_extract(filename, "[^_]+$"), ".xlsx"))
+}
 
